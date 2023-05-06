649ccc8c0dd6cb9aad2360b9e1cff7ae65d3c7e9
JzGamex <132711567+JzGamex@users.noreply.github.com>
2023-05-05 20:06:52 -0600

    Update departments.php

--- departments.php
+++ departments.php
@@ -1,67 +1,72 @@
 <header>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-  <?php
-  include('header.php');
-  ?>
-</header>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>St. John's Junior College</title>
-    <link rel="icon" type="image" href="https://i.imgur.com/Se45RL3.png">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
-</head>
-<body style="background-color:lightgrey">
-<section class="departments-section pt-5">
-  <div class="container-fluid pt-4">
-    <div class="row pt-3">
-      <div class="col-12 bg-primary p-5" style="min-height: 70vh">
-        <div class="department-card">
-          <div class="department-icon"><i class="bi bi-briefcase"></i></div>
-          <h3>Business</h3>
-          <p>Our business department offers programs in accounting, finance, marketing, and more.</p>
-          <a href="programs.php?department=bus" class="btn btn-light">Learn More</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <?php
+    include('header.php');
+    ?>
+  </header>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>St. John's Junior College</title>
+      <link rel="icon" type="image" href="https://i.imgur.com/Se45RL3.png">
+      <!-- Bootstrap CSS -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
+  </head>
+  <body style="background-color:lightgrey">
+  <section class="departments-section pt-5">
+    <div class="container-fluid pt-4">
+      <div class="row pt-3">
+        <div class="col-12 p-5" style="min-height: 70vh; background-image: url('https://www.technocrazed.com/wp-content/uploads/2015/12/Blue-Wallpaper-For-Background-1.jpg');">
+          <div class="department-card">
+            <div class="department-icon"><i class="bi bi-briefcase"></i></div>
+            <h3><b>Business</b></h3>
+            <p>Our business department offers programs in accounting, finance, marketing, and more.</p>
+            <a href="programs.php?department=bus" class="btn btn-light">Learn More</a>
+            <img src="https://images.businessnewsdaily.com/app/uploads/2019/03/25101809/Write-a-business-plan-1.png" style = "float: right" width="500" height="400">
+          </div>
         </div>
-      </div>
-      <div class="col-12 bg-secondary p-5" style="min-height: 70vh">
-        <div class="department-card">
-          <div class="department-icon"><i class="bi bi-laptop"></i></div>
-          <h3>Computer Science</h3>
-          <p>Our computer science department offers programs in software development, data science, and more.</p>
-          <a href="programs.php?department=cs" class="btn btn-light">Learn More</a>
+        <div class="col-12 p-5" style="min-height: 70vh; background-image: url('https://static.vecteezy.com/system/resources/previews/001/987/580/original/abstract-diagonal-white-grey-background-and-texture-free-vector.jpg');">
+          <div class="department-card">
+            <div class="department-icon"><i class="bi bi-laptop"></i></div>
+            <h3><b>Computer Science</b></h3>
+            <p>Our computer science department offers programs in software development, data science, and more.</p>
+            <a href="programs.php?department=cs" class="btn btn-light">Learn More</a>
+            <img src="https://favtutor.com/resources/images/uploads/randoms_920_x_470-5_(1).png" style = "float: right" width="650" height="350">
+          </div>
         </div>
-      </div>
-      <div class="col-12 bg-success p-5" style="min-height: 70vh">
-        <div class="department-card">
-          <div class="department-icon"><i class="bi bi-book"></i></div>
-          <h3>Humanities & Education</h3>
-          <p>Our humanities & education department offers programs in English, history, education, and more.</p>
-          <a href="programs.php?department=humedu" class="btn btn-light">Learn More</a>
+        <div class="col-12 p-5" style="min-height: 70vh; background-image: url('https://wallpaper-house.com/data/out/10/wallpaper2you_388492.jpg');">
+          <div class="department-card">
+            <div class="department-icon"><i class="bi bi-book"></i></div>
+            <h3><b>Humanities & Education</b></h3>
+            <p>Our humanities & education department offers programs in English, history, education, and more.</p>
+            <a href="programs.php?department=humedu" class="btn btn-light">Learn More</a>
+            <img src="https://www.conference2go.com/wp-content/uploads/2021/04/education-2.png" style = "float: right" width="500" height="400">
+          </div>
         </div>
-      </div>
-      <div class="col-12 bg-danger p-5" style="min-height: 70vh">
-        <div class="department-card">
-          <div class="department-icon"><i class="bi bi-calculator"></i></div>
-          <h3>Math & Science</h3>
-          <p>Our math & science department offers programs in mathematics, physics, biology, and more.</p>
-          <a href="programs.php?department=mthsci" class="btn btn-light">Learn More</a>
+        <div class="col-12 p-5" style="min-height: 70vh; background-image: url('https://jooinn.com/images/red-background-11.png');">
+          <div class="department-card">
+            <div class="department-icon"><i class="bi bi-calculator"></i></div>
+            <h3><b>Math & Science</b></h3>
+            <p>Our math & science department offers programs in mathematics, physics, biology, and more.</p>
+            <a href="programs.php?department=mthsci" class="btn btn-light">Learn More</a>
+            <img src="https://www.vnaya.com/assets/images/studentmath.png" style = "float: right" width="550" height="400">
+          </div>
         </div>
-      </div>
-      <div class="col-12 p-5" style="min-height: 70vh; background-color: yellow;">
-        <div class="department-card">
-          <div class="department-icon"><i class="bi bi-calculator"></i></div>
-          <h3>Social Science</h3>
-          <p>Our math & science department offers programs in mathematics, physics, biology, and more.</p>
-          <a href="programs.php?department=soc" class="btn btn-light">Learn More</a>
+        <div class="col-12 p-5" style="min-height: 70vh; background-image: url('https://cdn.wallpapersafari.com/62/24/JszirM.jpg');">
+          <div class="department-card">
+            <div class="department-icon"><i class="bi bi-calculator"></i></div>
+            <h3><b>Social Science</b></h3>
+            <p>Our math & science department offers programs in mathematics, physics, biology, and more.</p>
+            <a href="programs.php?department=soc" class="btn btn-light">Learn More</a>
+            <img src="https://www.planstreetinc.com/wp-content/uploads/2021/01/social-work-case-management-guide.png" style = "float: right" width="500" height="450">
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+  </section>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+  </html>
